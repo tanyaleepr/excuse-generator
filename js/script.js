3d14f667dcdd9775ba5f2a2cd65cb2e0543895e8
tanyaleepr <tanyaleepr@gmail.com>
2022-03-27 15:44:17 -0500

    final touch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var NumberOfWords = 50;
+var NumberOfWords = 52;
 var words = new BuildArray(NumberOfWords);
 
 // Use the following variables to 
@@ -42,17 +42,19 @@ words[36] = "I have Jury Duty"
 words[37] = "My child is sick"
 words[38] = "Sorry, my phone died"
 words[39] = "My bank blocked my accounts, cannot get money out"
-words[40] = "I haven't showered today, there is no water in my place"
+words[40] = "I haven't showered today, there is no water at my place"
 words[41] = "I had a nightmare about that day, I rather stay home and be safe"
-words[42] = ""
-words[43] = ""
-words[44] = ""
-words[45] = ""
-words[46] = ""
-words[47] = ""
-words[48] = ""
-words[49] = ""
-words[50] = ""
+words[42] = "I'm going to play Golf instead, just like our President"
+words[43] = "I asked my mommy and she told me no"
+words[44] = "Sorry, I am Billie Eillish official impersonator, can't go"
+words[45] = "When you guys start to pay me my worth I will arrive on time"
+words[46] = "Sorry I missed it, I really did not wanted to go"
+words[47] = "I asked my ancestors about that and they told me it was a terrible idea so I left"
+words[48] = "I tried to go but the best friends neighbors sister's niece had a terrible problem that only I could resolve"
+words[49] = "Trust me, you don't want to know"
+words[50] = "Fonsi told me to Despacito"
+words[51] = "A chicken attacked my mother"
+words[52] = "A deer broke into my house and I’m waiting for the insurance man"
 
 
 
